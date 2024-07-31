26c5c293d8f0775a2f327ea2b063d8568af25870
Ali Hasnain <alihasnainhfd@gmail.com>
2024-07-31 17:14:27 +0500

    laravel reverb

--- app/Events/TestEvent.php
+++ app/Events/TestEvent.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class TestEvent implements ShouldBroadcastNow
+class TestEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
